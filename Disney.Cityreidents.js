2ac7428adc9f9cad3c07e6be6b6124946386e416
Cityresidents <106128047+Cityresidents@users.noreply.github.com>
2022-08-12 15:56:48 +0800

    Update Disney.Cityreidents.js

--- Disney.Cityreidents.js
+++ Disney.Cityreidents.js
@@ -42,7 +42,7 @@ const STATUS_TIMEOUT = -1
 const STATUS_ERROR = -2
 
 const DEFAULT_OPTIONS = {
-  title: '𝗦𝘁𝗿𝗲𝗮𝗺𝗶𝗻𝗴 𝗗𝗶𝘀𝗻𝗲𝘆+ 解锁检测',
+  title: '𝗦𝘁𝗿𝗲𝗮𝗺𝗶𝗻𝗴 𝗗𝗶𝘀𝗻𝗲𝘆+  解锁检测',
   availableContent: '支持解锁 𝗗𝗶𝘀𝗻𝗲𝘆+ ➟ #REGION_FLAG# #REGION_NAME#',
   availableIcon: undefined,
   availableIconColor: undefined,
